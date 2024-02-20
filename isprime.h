264d72cf5c5bf126057e71f454495d3e81b219a4
Emrah Akgül <45803976+emrhAkgl@users.noreply.github.com>
2024-02-20 19:59:31 +0300

    Update isprime.h

--- isprime.h
+++ isprime.h
@@ -1,9 +1,27 @@
 #ifndef ISPRIME_H
 #define ISPRIME_H
 
-#include <stdio.h>
 #include <stdlib.h>
 
-int is_prime(long long end);
+int is_prime(long long end)
+{
+	int check;
+	long long start = 2;
+        if (end == 1 || end == 2)
+                return 1;
+
+        if (end < start)
+                return -1;
+    
+        
+        for(;start < end; start++) {
+                if (end %start == 0) {
+                        return 0;
+                }
+        }
+
+        return 1;
+}
+
 
 #endif
